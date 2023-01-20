e51fa29ac8a299375b779b2872a82778192055bc
Alejandro Lima Maldonado <allima69@gmail.com>
2023-01-20 00:47:12 -0400

    Modificacion rutas

--- controllers/User.js
+++ controllers/User.js
@@ -2,7 +2,7 @@ const User = require("../models/User");
 const catchAsync = require("../utils/catchAsync");
 
 const getAllUsers = catchAsync(async (req, res) => {
-    const products = await User.find();
+    const users = await User.find();
     
     res.status(200).json({
         status: "ok",
